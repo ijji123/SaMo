8fba36e9c0caec6947909fe9b0501a4d439714bd
ijji123 <44453494+ijji123@users.noreply.github.com>
2020-12-11 19:35:24 +0100

    Add files via upload
    please don't let me down

--- AudioController.py
+++ AudioController.py
@@ -2,16 +2,16 @@ from omxplayer.player import OMXPlayer
 import MotorController as motor
 import RPi.GPIO as GPIO
 import time
-import sys
 import os
 from threading import Thread
+import multiprocessing
 
 SongList = ["Musik/Klip1.wav", "Musik/Klip2.wav","Musik/Klip3.wav","Musik/Klip4.wav","Musik/Klip5.wav","Musik/Klip6.wav","Musik/Klip7.wav","Musik/Klip8.wav","Musik/Klip9.wav" ]# Faerdig
 SystemAudioList = ["SystemLyde/taend.wav", "SystemLyde/bluetoothforbundet.wav", "SystemLyde/bluetoothtaend1.wav", "SystemLyde/bluetoothsluk.wav", "sluk.wav"] # Faerdig
 AudioBookList= ["Lydboeger/DTBBAudio3Seffekter.mp3"]
 
-motor_thread = Thread(target=motor.TBBRotation, daemon=True)
-
+#motor_thread = Thread(target=motor.TBBRotation, daemon=True)
+motorprocess = multiprocessing.Process(target=motor.TBBRotation)
 
 def playSystemSound(command): # Her skal der ikke motor respons til, faerdig as is
     string = str(command)
@@ -41,10 +41,11 @@ def playSequence(seq):
         fileNumber = int(file)
         motor_thread =Thread(target=motor.act, args=(fileNumber,), daemon=True)
         motor_thread.start()
+        
         player = OMXPlayer(SongList[fileNumber])
         #motor.act(fileNumber)
-        motor_thread.join()
-#        time.sleep(4)
+       # motor_thread.join()
+        time.sleep(4)
         
 def playAudioBook(command):
     string = str(command)
@@ -53,14 +54,24 @@ def playAudioBook(command):
     string = string.replace("'", "")
     number = int(string)
     player = OMXPlayer(AudioBookList[number])
+    motorprocess.start()
     #motor.TBBRotation() #seneste
-#    motor_thread = Thread(target=motor.TBBRotation)
-    motor_thread.start()
-    notor_thread.join()
+#   motor_thread = Thread(target=motor.TBBRotation)
+    #motor_thread.start()
+    #motor_thread.join()
+
 
 def killAudio():
     quitCommand = "pkill omxplayer"
     os.system(quitCommand)
 
 def killMotor():
-    quit()
+    #quitCommand = "pkill -f MotorController.py"
+    #os.system(quitCommand)
+    #print("reached audio")
+    #motor.kill()
+    #sys.exit(0)
+    #raise SystemExit(0)
+    #quit()
+    #return
+    motorprocess.terminate()
--- MotorController.py
+++ MotorController.py
@@ -3,6 +3,7 @@ import time
 import sys
 from threading import Thread
 
+
 def setup():
     GPIO.setmode(GPIO.BOARD)
     #GPIO.setwarnings(False)
@@ -63,6 +64,7 @@ def frontHalfRotation():
     #GPIO.setwarnings(False)
 
     ControlPin = [7,11,13,15]
+    
  
     # Set all pins as output
     for pin in ControlPin:
@@ -86,10 +88,42 @@ def frontHalfRotation():
                 GPIO.output(ControlPin[pin], seqA[halfstep] [pin])
                 time.sleep(0.0002)
 
+def frontHalfRotationB():
+    GPIO.setmode(GPIO.BOARD)
+    #GPIO.setwarnings(False)
+
+    ControlPinB = [16,18,22,37]
+    
+ 
+    # Set all pins as output
+    for pin in ControlPinB:
+        GPIO.setup(pin,GPIO.OUT)
+        GPIO.output(pin, 0)
+
+        seqA = [
+            [1,0,0,0],
+            [1,1,0,0],
+            [0,1,0,0],
+            [0,1,1,0],
+            [0,0,1,0],
+            [0,0,1,1],
+            [0,0,0,1],
+            [1,0,0,1]
+         ]
+ 
+    for i in range(256):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(ControlPinB[pin], seqA[halfstep] [pin])
+                time.sleep(0.0002)
 
 def frontFullRotation():
     frontHalfRotation()
     frontHalfRotation()
+
+def frontFullRotationB():
+    frontHalfRotationB()
+    frontHalfRotationB()
     
 
 def backHalfRotation():
@@ -123,28 +157,112 @@ def backHalfRotation():
                     time.sleep(0.0002)
 #GPIO.cleanup()
 
+def backHalfRotationB():
+    GPIO.setmode(GPIO.BOARD)
+    #GPIO.setwarnings(False)
+
+    ControlPinB = [16,18,22,37]
+ 
+    # Set all pins as output
+    for pin in ControlPinB:
+        GPIO.setup(pin,GPIO.OUT)
+        GPIO.output(pin, 0)
+
+        seqA = [
+            [1,0,0,0],
+            [1,1,0,0],
+            [0,1,0,0],
+            [0,1,1,0],
+            [0,0,1,0],
+            [0,0,1,1],
+            [0,0,0,1],
+            [1,0,0,1]
+         ]
+
+    seqB = seqA[::-1]
+
+    for i in range(256):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(ControlPinB[pin], seqB[halfstep] [pin])
+                    time.sleep(0.0002)
+
+def cleanUp():
+    GPIO.cleanup()
+
+
 def backFullRotation():
     backHalfRotation()
     backHalfrotation()
 
+def backFullRotationB():
+    backHalfRotationB()
+    backHalfrotationB()
 
+# def TBBRotation():
+#    frontHalfRotation() # "De tre bukke bruse"
+#    backHalfRotation() # Introduktion
+#    frontHalfRotation() # "Lille bukke bruse"
+#    frontHalfRotation() # "Mellemste bukke bruse"
+#    backHalfRotation() # "Store bukke bruse"
+#    backHalfRotation() # "Gammel gnaven trold"
+#    backHalfRotation()
+    
 def TBBRotation(): #seneste
-   frontHalfRotation()
-   backHalfRotation()
-   time.sleep(1)
-   frontHalfRotation()
-   backHalfRotation()
-   time.sleep(1)
-   frontHalfRotation()
-   frontHalfRotation()
+   frontHalfRotation() # "De tre bukke bruse"
+   backHalfRotationB() # Introduktion
+   time.sleep(9)
+   frontHalfRotation() # "Lille bukke bruse"
+   frontHalfRotationB() # "Mellemste bukke bruse"
+   backHalfRotation() # "Store bukke bruse"
+   time.sleep(10)
+   backHalfRotation() # "Gammel gnaven trold"
+   backHalfRotationB()
+   time.sleep(4)
+   frontHalfRotationB() # "Trip"
+   frontHalfRotation() # "Trip, trip"
+   time.sleep(2)
+   frontHalfRotation() #"Hvem er det der tramper paa min bro"
+   time.sleep(6)
+   frontHalfRotationB() #"Det er mig, den lille bukke bruse"
+   time.sleep(3)
+   frontHalfRotation() # "Jeg er saa sulten!"
+   time.sleep(2)
+   frontHalfRotation() # "Nu kommer jeg og spiser dig!"
+   time.sleep(4.5)
+   frontHalfRotationB() # "Jeg er saa lille og tynd"
+   backHalfRotationB()
+   time.sleep(3)
+   frontHalfRotation() # "Saa lad gaa!"
+   time.sleep(4)
+   frontHalfRotationB() # tramp tramp
+   frontHalfRotation() # tramp
+   time.sleep(5)
+   backHalfRotationB() # "Trip"
+   backHalfRotation() # "Trap, trip"
+   time.sleep(2)
+   frontHalfRotation() # "Hvem er det der tramper paa min bro"
+   time.sleep(2)
+   backHalfRotationB() # "Det er mig, den mellemste bukke bruse"
+   time.sleep(5)
+   frontHalfRotation() # "Nu kommer jeg og spiser dig"
+   time.sleep(10)
+   backHalfRotationB() # "Nej, det er ikke mig!"
+   frontHalfRotationB()
+   time.sleep(5)
+   frontHalfRotationB() #tramp 
+   frontHalfRotation() #tramp tramp // Herfra er det ikke 100p skarpt, fortsaet med tests. Denne version er ikke testet! 
+   time.sleep(3.5)
+   frontHalfRotationB() #"Tramp,
+   frontHalfRotation() #"Tramp, tramp"
    time.sleep(0.2)
-   backHalfRotation()
-   time.sleep(0.2)
-   backHalfRotation()
-   time.sleep(0.2)
-   backHalfRotation()
-   
+   frontHalfRotation() # "Hvem er det der tramper paa min bro!"
+   time.sleep(3)
+   frontHalfRotationB() # "Det er mig, den store bukke bruse"
+   time.sleep(2.5)
+   frontHalfRotation() # "Endeligt!"
+
+
 def startThread():
     TBB_thread = Thread(target=TBBRotation)
     TBB_thread.start()
-
